39bce483ef56e9db8c83422754b1deca11449777
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-09-12 11:51:40 +0700

    перенес логику получениязаявок на регистрацию в хранилище

--- src/pages/Requests/index.tsx
+++ src/pages/Requests/index.tsx
@@ -2,38 +2,32 @@ import React from 'react';
 import { EmptyResult } from '../../components/Stub';
 import type { ConfirmationRequest } from '../../models/Request';
 import { RequestCard } from '../../components/Card/Request';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { clearSession } from '../../store/Auth/authReducer';
+import { fetchRequests } from '../../store/Requests/requestAction';
+import { ErrorToast } from '../../components/Toast';
+import { useAppSelector } from '../../hooks/useAppSelector';
 
 export const RequestsPage = () => {
     const [requestList, setRequestList] = React.useState<Array<ConfirmationRequest>>([]);
     const [isLoaded, setLoaded] = React.useState<boolean>(false);
+    const { requests } = useAppSelector((state) => state.requestReducer);
     const dispatch: any = useDispatch();
-    const fetchRequests = async () => {
-        setLoaded(false);
+    const fetchRequestList = async () => {
         try {
-            const response = await axios({
-                url: `${import.meta.env.API_URL}/user/confirmation/list`,
-                method: 'GET',
-                headers: {
-                    Authorization: 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'),
-                },
-            });
-            const data = await response.data;
-            setRequestList(data);
+            setLoaded(false);
+            dispatch(fetchRequests());
+            setLoaded(true);
         } catch (error) {
-            if (error.status === 401) {
-                dispatch(clearSession());
-            }
+            ErrorToast('Не удалось получить данные');
         }
-        setLoaded(true);
     };
     React.useEffect(() => {
         if (!isLoaded) {
-            fetchRequests();
+            fetchRequestList();
+        } else {
+            setRequestList(requests);
         }
-    }, [isLoaded]);
+    }, [isLoaded, requests]);
     return (
         <main className='w-full h-auto flex flex-col px-6 py-10'>
             <div className='flex flex-col mx-auto max-w-[768px] w-full gap-10 justify-between'>
